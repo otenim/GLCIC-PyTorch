da8fc824ed31562404125bdd1ae97eb8888ee4a4
tsukada <tsukada@silver.local>
2018-08-11 17:08:42 +0900

    add 'places2' mode to Global Discriminator

--- models.py
+++ models.py
@@ -5,7 +5,9 @@ from layers import Flatten, Concatenate
 
 
 class CompletionNetwork(nn.Module):
+
     def __init__(self):
+
         super(CompletionNetwork, self).__init__()
         # input_shape: (None, 3, img_h, img_w)
         self.conv1 = nn.Conv2d(3, 64, kernel_size=5, stride=1, padding=2)
@@ -77,6 +79,7 @@ class CompletionNetwork(nn.Module):
         # output_shape: (None, 3, img_h. img_w)
 
     def forward(self, x):
+
         x = self.bn1(self.act1(self.conv1(x)))
         x = self.bn2(self.act2(self.conv2(x)))
         x = self.bn3(self.act3(self.conv3(x)))
@@ -98,7 +101,9 @@ class CompletionNetwork(nn.Module):
 
 
 class LocalDiscriminator(nn.Module):
+
     def __init__(self, input_shape):
+
         super(LocalDiscriminator, self).__init__()
         self.input_shape = input_shape
         self.output_shape = (1024,)
@@ -134,6 +139,7 @@ class LocalDiscriminator(nn.Module):
         # output_shape: (None, 1024)
 
     def forward(self, x):
+
         x = self.bn1(self.act1(self.conv1(x)))
         x = self.bn2(self.act2(self.conv2(x)))
         x = self.bn3(self.act3(self.conv3(x)))
@@ -144,13 +150,17 @@ class LocalDiscriminator(nn.Module):
 
 
 class GlobalDiscriminator(nn.Module):
-    def __init__(self, input_shape):
+
+    def __init__(self, input_shape, mode='celeba'):
+
         super(GlobalDiscriminator, self).__init__()
+        self.mode = mode
         self.input_shape = input_shape
         self.output_shape = (1024,)
         self.img_c = input_shape[0]
         self.img_h = input_shape[1]
         self.img_w = input_shape[2]
+
         # input_shape: (None, img_c, img_h, img_w)
         self.conv1 = nn.Conv2d(self.img_c, 64, kernel_size=5, stride=2, padding=2)
         self.bn1 = nn.BatchNorm2d(64)
@@ -171,31 +181,53 @@ class GlobalDiscriminator(nn.Module):
         self.conv5 = nn.Conv2d(512, 512, kernel_size=5, stride=2, padding=2)
         self.bn5 = nn.BatchNorm2d(512)
         self.act5 = nn.ReLU()
-        # input_shape: (None, 512, img_h//32, img_w//32)
-        in_features = 512 * (self.img_h//32) * (self.img_w//32)
-        self.flatten6 = Flatten()
-        # input_shape: (None, 512 * img_h//32 * img_w//32)
-        self.linear6 = nn.Linear(in_features, 1024)
-        self.act6 = nn.ReLU()
+
+        if self.mode == 'celeba':
+            # input_shape: (None, 512, img_h//32, img_w//32)
+            in_features = 512 * (self.img_h//32) * (self.img_w//32)
+            self.flatten6 = Flatten()
+            self.linear6 = nn.Linear(in_features, 1024)
+            self.act6 = nn.ReLU()
+        elif self.mode == 'places2':
+            # input_shape: (None, 512, img_h//32, img_w//32)
+            self.conv6 = nn.Conv2d(512, 512, kernel_size=5, stride=2, padding=2)
+            self.bn6 = nn.BatchNorm2d(512)
+            self.act6 = nn.ReLU()
+            # input_shape: (None, 512, img_h//64, img_w//64)
+            in_features = 512 * (self.img_h//64) * (self.img_w//64)
+            self.flatten7 = Flatten()
+            self.linear7 = nn.Linear(in_features, 1024)
+            self.act7 = nn.ReLU()
+        else:
+            raise ValueError('Unknown mode \'%s\'.' % self.mode)
         # output_shape: (None, 1024)
 
+
     def forward(self, x):
+
         x = self.bn1(self.act1(self.conv1(x)))
         x = self.bn2(self.act2(self.conv2(x)))
         x = self.bn3(self.act3(self.conv3(x)))
         x = self.bn4(self.act4(self.conv4(x)))
         x = self.bn5(self.act5(self.conv5(x)))
-        x = self.act6(self.linear6(self.flatten6(x)))
+        if self.mode == 'celeba':
+            x = self.act6(self.linear6(self.flatten6(x)))
+        else: # 'places2'
+            x = self.bn6(self.act6(self.conv6(x)))
+            x = self.act7(self.linear7(self.flatten7(x)))
         return x
 
 
 class ContextDiscriminator(nn.Module):
-    def __init__(self, local_input_shape, global_input_shape):
+
+    def __init__(self, local_input_shape, global_input_shape, mode='celeba'):
+
         super(ContextDiscriminator, self).__init__()
+        self.mode = mode
         self.input_shape = [local_input_shape, global_input_shape]
         self.output_shape = (1,)
         self.model_ld = LocalDiscriminator(local_input_shape)
-        self.model_gd = GlobalDiscriminator(global_input_shape)
+        self.model_gd = GlobalDiscriminator(global_input_shape, mode=self.mode)
         # input_shape: [(None, 1024), (None, 1024)]
         in_features = self.model_ld.output_shape[-1] + self.model_gd.output_shape[-1]
         self.concat1 = Concatenate(dim=-1)
